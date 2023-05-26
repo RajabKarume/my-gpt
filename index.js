3ab9e0fbb78ece6d75e04baa5a77bee22644f4cf
Rajab Karume <rkarume@gmail.com>
2023-05-26 10:38:11 +0300

    ✨created listening port

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
+const express = require("express")
 require('dotenv').config()
 
 const configuration = new Configuration({
@@ -7,13 +8,20 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-async function callApi(){
+const app = express()
+const port = 3080
+
+app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "Say this is a test",
         max_tokens: 7,
         temperature: 0,
-      });
+    })
     console.log(response.data.choices[0].text)
-}
-callApi()
+    res.json({data: response.data})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http:// localhost:${port}`)
+})
